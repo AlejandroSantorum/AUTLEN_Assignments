7f7d58886102e59245ecb36c074704b2e8e432e5
Alejandro Santorum <alejandro.santorum@gmail.com>
2019-11-18 01:34:45 +0100

    cd

--- 1p/srclib/utils.c
+++ 1p/srclib/utils.c
@@ -12,12 +12,23 @@
 #include "utils.h"
 
 
-/*
+/******************************************************************
  *
  * Utilities to make some operations with squared matrix
  *
- */
+******************************************************************/
 
+/**
+ sq_mtx_mult
+ Input:
+     uint8_t **mtxA: squared matrix of uint8_t
+     uint8_t **mtxB: squared matrix of uint8_t
+     uint8_t **res: squared matrix of uint8_t
+     size_t size: matrix sizes
+ Returns:
+     Void.
+     It computes on matrix res the matrix multiplication A*B
+*/
 void sq_mtx_mult(uint8_t **mtxA, uint8_t **mtxB, uint8_t **res, size_t size){
     if (!mtxA || !mtxB || !res){
         return;
@@ -33,9 +44,19 @@ void sq_mtx_mult(uint8_t **mtxA, uint8_t **mtxB, uint8_t **res, size_t size){
 
     sq_mtx_cpy(res, res_tmp, size);
     sq_mtx_delete(res_tmp, size);
-
 }
 
+/**
+ sq_mtx_power
+ Input:
+     uint8_t **mtx: squared matrix of uint8_t
+     uint8_t **res: squared matrix of uint8_t
+     size_t size: matrix sizes
+     size_t power: power value
+ Returns:
+     Void.
+     It computes on matrix res the result of matrix mtx to the 'power'
+*/
 void sq_mtx_power (uint8_t **mtx, uint8_t **res, size_t size, size_t power) {
     if (!mtx || !res){
         return;
@@ -56,6 +77,14 @@ void sq_mtx_power (uint8_t **mtx, uint8_t **res, size_t size, size_t power) {
     }
 }
 
+/**
+ sq_mtx_new
+ Input:
+     size_t sz: matrix size
+ Returns:
+     uint8_t double pointer: pointer to a squared uint8_t matrix of size 'sz'
+     with allocated memory
+*/
 uint8_t **sq_mtx_new(size_t sz){
     uint8_t **mtx;
     mtx = calloc(sz, sizeof(uint8_t *));
@@ -65,6 +94,15 @@ uint8_t **sq_mtx_new(size_t sz){
     return mtx;
 }
 
+/**
+ sq_mtx_delete
+ Input:
+     uint8_t **mtx: squared matrix of uint8_t
+     size_t sz: matrix size
+ Returns:
+     Void.
+     It frees the allocated memory for a given matrix
+*/
 void sq_mtx_delete(uint8_t **mtx, size_t sz){
     if (mtx) {
         for (size_t i = 0; i < sz; i++) {
@@ -78,6 +116,19 @@ void sq_mtx_delete(uint8_t **mtx, size_t sz){
     }
 }
 
+/**
+ sq_mtx_cpy
+ Input:
+     uint8_t **dest: squared matrix of uint8_t
+     uint8_t **src: squared matrix of uint8_t
+     size_t sz: matrix sizes
+ Returns:
+     Void.
+     It copies the src matrix on dest matrix.
+ Advanced description:
+     It copies src matrix on dest matrix. Both matrices have to have
+     pre-allocated memory.
+*/
 void sq_mtx_cpy(uint8_t **dest, uint8_t **src, size_t sz){
     if(!dest || !src){
         return;
@@ -87,6 +138,17 @@ void sq_mtx_cpy(uint8_t **dest, uint8_t **src, size_t sz){
     }
 }
 
+/**
+ fprintf_sq_mtx
+ Input:
+     FILE *f: file pointer
+     uint8_t **mtx: squared matrix of uint8_t
+     size_t sz: matrix size
+ Returns:
+     Number of printed characters
+ Advanced description:
+     It outputs on FILE f the representation of the given matrix
+*/
 int fprintf_sq_mtx(FILE *f, uint8_t **mtx, size_t sz) {
     if (!f || !mtx) return 0;
     int chars = 0;
@@ -103,13 +165,25 @@ int fprintf_sq_mtx(FILE *f, uint8_t **mtx, size_t sz) {
 }
 
 
-/*
+
+/******************************************************************
  *
  * Utilities to translate from the structures in afnd.h to a more simplified
  * structure that we know better.
  *
- */
+*******************************************************************/
 
+/**
+ get_alphabet
+ Input:
+     AFND *afnd: NDFA pointer
+     size_t *arr_sz: pointer to a size_t
+ Returns:
+     The NDFA alphabet (char**)
+ Advanced description:
+     It returns the NDFA alphabet, that is an array of strings because each
+     symbol could be a string; and it returns the alphabet size in arr_sz
+*/
 char **get_alphabet(AFND *afnd, size_t *arr_sz){
     if (!afnd) {
         return NULL;
@@ -122,6 +196,19 @@ char **get_alphabet(AFND *afnd, size_t *arr_sz){
     return alphabet;
 }
 
+/**
+ get_initial_states
+ Input:
+     AFND *afnd: NDFA pointer
+     size_t *arr_sz: pointer to a size_t
+ Returns:
+     The NDFA initial states
+ Advanced description:
+     It returns the NDFA initial states, represented as an array of size equal to
+     the number of NDFA total states. The initial states are marked with 1's,
+     otherwise 0, i.e., state 3 is initial <=> array[3] == 1. It also returns the
+     total number of states in arr_sz
+*/
 uint8_t *get_initial_states(AFND *afnd, size_t *arr_sz){
     if (!afnd) {
         return NULL;
@@ -135,6 +222,19 @@ uint8_t *get_initial_states(AFND *afnd, size_t *arr_sz){
     return initial_states;
 }
 
+/**
+ get_final_states
+ Input:
+     AFND *afnd: NDFA pointer
+     size_t *arr_sz: pointer to a size_t
+ Returns:
+     The NDFA final states
+ Advanced description:
+     It returns the NDFA final states, represented as an array of size equal to
+     the number of NDFA total states. The final states are marked with 1's,
+     otherwise 0, i.e., state 5 is final <=> array[5] == 1. It also returns the
+     total number of states in arr_sz
+*/
 uint8_t *get_final_states(AFND *afnd, size_t *arr_sz){
     if (!afnd) {
         return NULL;
@@ -148,6 +248,24 @@ uint8_t *get_final_states(AFND *afnd, size_t *arr_sz){
     return final_states;
 }
 
+/**
+ get_nfa_transition_table
+ Input:
+     AFND *afnd: NDFA pointer
+ Returns:
+     The NDFA transition table
+ Advanced description:
+     It returns the NDFA transition table, represented as a 3-dimensional matrix.
+     i.e, each row represents a state (ST), each column a symbol (SY) and each
+     element of the table is an array of states that can be visited by state ST
+     with input symbol SY. For eaxmple:
+     __________________________
+     |  |  'a'  |  'b'  | 'c' |
+     |q0|{q1,q2}| {q1}  |  -  |  From q0 you can visit q1 or q2 with symb. 'a'
+     |q1|  {q3} |{q1,q2}|  -  |
+     |q2|  -    |  {q2} | {q0}|
+     --------------------------
+*/
 uint8_t ***get_nfa_transition_table(AFND *afnd){
     size_t nstates = AFNDNumEstados(afnd);
     size_t nsym = AFNDNumSimbolos(afnd);
@@ -171,6 +289,16 @@ uint8_t ***get_nfa_transition_table(AFND *afnd){
     return trans_tb;
 }
 
+/**
+ delete_nfa_transition_table
+ Input:
+     uint8_t ***trans_tb: NDFA transition table previously described
+     size_t nstates: NDFA total number of states
+     size_t nsym: alphabet total number of symbols
+ Returns:
+     Void.
+     It frees pre-allocated memory for transition table
+*/
 void delete_nfa_transition_table(uint8_t ***trans_tb, size_t nstates, size_t nsym){
     if(!trans_tb) {
         return;
@@ -192,6 +320,17 @@ void delete_nfa_transition_table(uint8_t ***trans_tb, size_t nstates, size_t nsy
     trans_tb = NULL;
 }
 
+/**
+ get_lambda_clausure
+ Input:
+     AFND *afnd: NDFA pointer
+     size_t *tb_sz: pointer to a size_t value
+ Returns:
+     The lambda clausure matrix
+ Advanced description:
+     It returns the lambda clausure matrix, represented as a uint8_t matrix.
+     It also returns the matrix size (it's squared) in tb_sz.
+*/
 uint8_t **get_lambda_clausure(AFND *afnd, size_t *tb_sz){
     AFND *c_afnd = AFNDCierraLTransicion(afnd);
     size_t nstates = AFNDNumEstados(c_afnd);
@@ -208,6 +347,27 @@ uint8_t **get_lambda_clausure(AFND *afnd, size_t *tb_sz){
 }
 
 
+
+/******************************************************************
+ *
+ * Utilities to translate from our structures to afnd.h structures
+ *
+*******************************************************************/
+
+/**
+ get_dfa_object
+ Input:
+     row *afd_table: row pointer = representation of the transition table
+     char **alphabet: alphabet precalculated with the get_alphabet function
+     size_t alph_sz: alphabet size
+     dfa_states: number of DFA states
+     nstates: number of NDFA states
+ Returns:
+     The AFND OBJECT (IN THIS CASE IT IS ALREADY A DFA)
+ Advanced description:
+     It creates the DFA from the precalculated transition table and alphabet.
+     It translates our structures to afnd.h structures
+*/
 AFND *get_dfa_object(row *afd_table, char **alphabet, size_t alph_sz, size_t dfa_states, size_t nstates){
 
     if (!afd_table || !alphabet) return NULL;
